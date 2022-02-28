04372bcfb10c9251fbceb4ca0dea9d74cfef8143
Skywalker5276 <100366106+Skywalker5276@users.noreply.github.com>
2022-02-28 11:13:36 -0500

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,15 @@
+input.onButtonPressed(Button.A, function () {
+    Sprite.change(LedSpriteProperty.Y, 1)
+})
+input.onGesture(Gesture.TiltLeft, function () {
+    Sprite.move(-1)
+})
+input.onButtonPressed(Button.B, function () {
+    Sprite.change(LedSpriteProperty.Y, -1)
+})
+input.onGesture(Gesture.TiltRight, function () {
+    Sprite.move(1)
+})
+let Sprite: game.LedSprite = null
+basic.showIcon(IconNames.Heart)
+Sprite = game.createSprite(2, 2)
